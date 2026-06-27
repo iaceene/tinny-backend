081ab9af0b720e9b9cba2e7ebe4afd4c0936368f
iaceene <yassineajagrou986@gmail.com>
2026-06-27 16:21:00 +0100

    v2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-export { default as Server } from './core/Server.js';
-export { default as TinnyServer } from './core/Server.js';
+import Server from "./core/Server.js";
+import type { ServerOptions } from "./core/types.js";
+
+export { Server };
+export default Server;
+
+export function createServer(options?: ServerOptions) {
+    return new Server(options ?? {});
+}
+
+export { VERSION } from "./utils/version.js";
 
 export type {
     ServerOptions,
@@ -16,23 +25,4 @@ export type {
     Decorator,
     DirFiles,
     ServFiles
-} from './core/types.js';
-
-export const VERSION = '1.0.0';
-export const NAME = 'tinny-backend';
-
-
-import Server from './core/Server.js';
-export default Server;
-
-export function createServer(options?: import('./core/types.js').ServerOptions): Server {
-    return new Server(options || {});
-}
-
-export const packageInfo = {
-    name: NAME,
-    version: VERSION,
-    description: 'A lightweight Node.js HTTP server framework',
-    homepage: 'https://github.com/iaceene/tinny-backend',
-    bugs: 'https://github.com/iaceene/tinny-backend/issues'
-};
+} from "./core/types.js";
